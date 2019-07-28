5fc0abffe0bdd18c1b1188b941428ff7ddba6e28
basebandit <evansonmwangi83@gmail.com>
2019-07-28 20:27:26 +0300

    :wrench: Load and decode config file from given filepath

---
+++ pkg/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Config struct {
+	ApiKey *string `json:"api_key"`
+}
+
+//LoadConfig loads the configuration file from the specified filepath
+func LoadConfig(filepath string) (*Config, error) {
+	//Get the config file
+	configFile, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+	config := &Config{}
+	err = json.Unmarshal(configFile, config)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
