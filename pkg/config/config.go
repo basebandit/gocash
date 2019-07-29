a47abfc8900f0f288a58c77d37ae2c71c6333d4f
basebandit <evansonmwangi83@gmail.com>
2019-07-29 16:14:11 +0300

    Retrieve currencies from currencies json file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,7 +7,7 @@ import (
 
 type Config struct {
 	ApiKey string `json:"api_key"`
-	Api string `json:"api"`
+	Api    string `json:"api"`
 }
 
 //LoadConfig loads the configuration file from the specified filepath
@@ -23,4 +23,19 @@ func LoadConfig(filepath string) (*Config, error) {
 		return nil, err
 	}
 	return config, nil
+}
+
+//GetCurrencies loads the currency json file from the specified filepath
+//and returns a list of (key,value) pairs of currencies
+func GetCurrencies(filepath string) (map[string]interface{}, error) {
+	currFile, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+	var currency map[string]interface{}
+	err = json.Unmarshal(currFile, &currency)
+	if err != nil {
+		return nil, err
+	}
+	return currency, nil
 }
