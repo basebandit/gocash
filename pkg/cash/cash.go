0923945e5b87778ef8cc17600d9bc33dc8b82a09
basebandit <evansonmwangi83@gmail.com>
2019-07-29 03:30:24 +0300

    :dollar: Parse base currency for use in conversion

--- pkg/cash/cash.go
+++ pkg/cash/cash.go
@@ -28,3 +28,17 @@ func ParseRates(data []byte) map[string]interface{} {
 	}
 	return r
 }
+
+
+//ParseBase retrieves base currency value from the decoded json response
+func ParseBase(data []byte) string {
+	var b string
+	res, err := unmarshalJSON(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if base, ok := res["base"]; ok {
+		b = base.(string)
+	}
+	return b
+}
